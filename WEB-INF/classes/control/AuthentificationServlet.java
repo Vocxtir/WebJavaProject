419d31502b90d8836212da63284057abf6b4ca7b
Vocxtir <rahman.victor@outlook.fr>
2016-02-23 11:01:22 +0100

    Change01

--- WEB-INF/classes/control/AuthentificationServlet.java
+++ WEB-INF/classes/control/AuthentificationServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.User;
+
 
 
 public class AuthentificationServlet extends HttpServlet {
--- WEB-INF/classes/model/UserSA.java
+++ WEB-INF/classes/model/UserSA.java
@@ -2,11 +2,7 @@ package model;
 
 public class UserSA {
 	public static void createUser (String username, String password) {
-		Avion camion = listAvion.get(mat);
-		if (cityLoc == null | camion != null)
-			throw new IllegalArgumentException("Ville ou camion non existant");
-		camion = new Avion (mat,brand,cityLoc);
-		listAvion.put(mat, camion);
+		User u = new User (username,password);
 		
 	}
 }
